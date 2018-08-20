9fdf4aca51c41b4902b967e75a6019e20a12be6c
hernandezpalo83 <hernandezpalo@gmail.com>
2018-08-20 11:13:23 +0200

    Inicial
    Inicial

---
+++ holamundo.py
@@ -0,0 +1,2 @@
+def f_holamundo():
+    print("Hola Mundo")
---
+++ ini.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+import os
+from holamundo import f_holamundo
+
+def menu():
+	"""
+	Función que limpia la pantalla y muestra nuevamente el menu
+	"""
+	os.system('clear') # NOTA para windows tienes que cambiar clear por cls
+	print ("Selecciona una opción")
+	print ("\t1 - Hola Mundo")
+	print ("\t2 - Sin funcion")
+	print ("\t3 - Sin funcion")
+	print ("\t9 - salir")
+    
+ 
+while True:
+	# Mostramos el menu
+	menu()
+ 
+	# solicituamos una opción al usuario
+	opcionMenu = input("inserta un numero valor >> ")
+    
+	if opcionMenu=="1":
+		f_holamundo()        
+	elif opcionMenu=="2":
+		print ("")
+		input("Has pulsado la opción 2...\npulsa una tecla para continuar")
+	elif opcionMenu=="3":
+		print ("")
+		input("Has pulsado la opción 3...\npulsa una tecla para continuar")
+	elif opcionMenu=="9":
+		break
+	else:
+		print ("")
+		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")

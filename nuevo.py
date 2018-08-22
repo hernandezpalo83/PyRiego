2c92b5a4c96eecc8a732a4d33becc22c8705650a
Javier Hernandez <javier.hernandez@sandav.local>
2018-08-22 08:47:49 +0200

    Inicial funciones pantalla LCD

--- nuevo.py
+++ nuevo.py
@@ -1,1 +1,7 @@
-pint ( ultimo tocado )
+from pyfirmata import Arduino, util, STRING_DATA
+board = Arduino('COM6')
+board.send_sysex( STRING_DATA, util.str_to_two_byte_iter('Hello!') )
+
+def msg( text ):
+    if text:
+        board.send_sysex( STRING_DATA, util.str_to_two_byte_iter( text ) )
--- pantalla.py
+++ pantalla.py
@@ -1,2 +1,7 @@
-def f_holamundo():
-    print("Pantalla")
+from pyfirmata import Arduino, util, STRING_DATA
+board = Arduino('COM6')
+board.send_sysex( STRING_DATA, util.str_to_two_byte_iter('Hello!') )
+
+def msg( text ):
+    if text:
+        board.send_sysex( STRING_DATA, util.str_to_two_byte_iter( text ) )
